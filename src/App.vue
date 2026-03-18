1806c80f82dbae09b710748d5a30f779ba3afab8
Dmitriy Dyakov <d.dyakov@evrone.ru>
2026-03-18 17:17:48 +0300

    improvements

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <style lang="scss" scoped>
 .container {
   height: 100%;
-  padding: 15px;
+  padding: var(--space-lg);
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
   border-radius: 10px;
   background-color: #F8F9FA;
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,10 +1,14 @@
 <template>
   <h1>Главная страница</h1>
 
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis reiciendis tenetur doloremque alias minus asperiores quo, repellendus corrupti labore voluptatibus quisquam non aut soluta deserunt, adipisci facilis dolore quos. Modi? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis reiciendis tenetur doloremque alias minus asperiores quo, repellendus corrupti labore voluptatibus quisquam non aut soluta deserunt, adipisci facilis dolore quos. Modi? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis reiciendis tenetur doloremque alias minus asperiores quo, repellendus corrupti labore voluptatibus quisquam non aut soluta deserunt, adipisci facilis dolore quos. Modi?</p>
+
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis reiciendis tenetur doloremque alias minus asperiores quo, repellendus corrupti labore voluptatibus quisquam non aut soluta deserunt, adipisci facilis dolore quos. Modi?</p>
+
+  <span class="text-small">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum temporibus nostrum at laudantium illum inventore nihil magni hic. Veniam quibusdam sint voluptate autem? Est, eligendi voluptas reiciendis ad minus libero!</span>
   <Wrapper />
 </template>
 
 <script setup lang="ts">
 import Wrapper from '@/components/layout/Wrapper.vue';
-
 </script>
